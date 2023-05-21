5a1546d045a1e0028844f713bd2f70883060f428
Victor Silva <victor.silvaPC@hotmail.com>
2023-05-21 13:17:24 -0300

    feat: :tada: finished the Hash Maps section

--- 05_table_hash/python/tablehash.py
+++ 05_table_hash/python/tablehash.py
@@ -6,3 +6,28 @@ book["cookies"] = 2.70
 
 print(book["milk"])
 print(book.get("cookies"))
+
+# Telephone Directory
+
+telephone_dict = dict()
+
+telephone_dict["Jenny"] = 8675309
+telephone_dict["emergency"] = 911
+
+print(telephone_dict["Jenny"])
+
+# vote
+
+voted = {}
+
+def verify_voter(name):
+  if voted.get(name):
+    print("Send Away")
+  else:
+    voted[name] = True
+    print("Let vote")
+
+verify_voter("tom")
+verify_voter("mike")
+verify_voter("mike")
+
