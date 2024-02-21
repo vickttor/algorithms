bb75ce6133cbad6398ed90326432e5a687c19a42
Victor Silva <victor.silvaPC@hotmail.com>
2024-02-21 19:47:44 -0300

    fix: remove the old progress

--- 03_recursion/javascript/03_case_base_and_recursive.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function countdown(number){
-  console.log(number);
-  if(number >= 1) return countdown(number - 1);
-  return;
-}
-
-countdown(10)
--- 03_recursion/python/03_recursion_factorial.py
+++
@@ -1,9 +0,0 @@
-# Calculate the factorial of a number
-
-def fat(i):
-  if i == 1:
-    return 1
-  else:
-    return i * fat(i - 1)
-
-print(fat(6)) # it returns 720
